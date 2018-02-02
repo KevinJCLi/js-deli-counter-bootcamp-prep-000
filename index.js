0254710899de762d7dad99d78ebc7299761b54f1
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 23:31:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine (katzDeliLine) {
   } else {
     var theCurrentLine = [];
     for (let i = 0; i < katzDeliLine.length; i++) {
-      theCurrentLine.push("1. " + katzDeliLine[i] + " ");
+      theCurrentLine.push(" 1. " + katzDeliLine[i]);
     }
     return `The line is currently: ${theCurrentLine}`;
   }
