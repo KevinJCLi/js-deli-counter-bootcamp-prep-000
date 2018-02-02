ee47e1a36637d996029b8af2de2127e2cd16b68a
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 23:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing () {
    servedCustomer = katzDeli[0];
   }
   katzDeli.shift();
-  return `Currently serving ${servedCustomer}`;
+  return servedCustomer;
 }
 
 function currentLine () {
