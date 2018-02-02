3f3302e3e5d9d3c748a2b6e1db2dd28dcb791f8b
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 21:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var katzDeli = [];
 
 function takeANumber (katzDeliLine, customerName) {
-  var linePosition, ;
-  katzDeli[katzDeliLine + 1] = customerName;
+  var linePosition = katzDeliLine + 1, 
+    ordinalIndicator;
   if (line position === 1) {
-    
+    ordinalIndicator = "st";
+  } else if (line position === 2) {
+    ordinalIndicator = "nd";
+  } else {
+    ordinalIndicator = "th"
   }
-  return `You are the ${katzDeliLine + 1}
+  return `You are the ${linePosition}${OrdinalIndicator} person in line`;
 }
