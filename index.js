fc5536fe6e88a45b7ed54dc13fbe101e209586c6
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 22:06:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeli = [];
 
 function takeANumber (katzDeliLine, customerName) {
-  var linePosition = katzDeliLine.length + 1, 
-    ordinalIndicator;
+  var linePosition = katzDeliLine.length + 1;
+  var ordinalIndicator;
   if (lineposition === 1) {
     ordinalIndicator = "st";
   } else if (lineposition === 2) {
