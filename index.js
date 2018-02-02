200714d42c703aa8b5f5bd8fe1fea2cf12b944da
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 21:52:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, customerName) {
+  var linePosition, ;
+  katzDeli[katzDeliLine + 1] = customerName;
+  if (line position === 1) {
+    
+  }
+  return `You are the ${katzDeliLine + 1}
+}
