0a1e85693dcf0f03208ed1a86f024a9ff1f6f7db
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 22:11:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber (katzDeliLine, customerName) {
   var linePosition = katzDeliLine.length + 1;
-  katzDeli[linePosition] = customerName;
+  katzDeli.push(customerName);
   return `Welcome, ${customerName}. You are number ${linePosition} in line.`;
 }
